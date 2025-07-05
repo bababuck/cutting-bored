c9813d9869a35ad99a9066eb55fc3a9b0e0886f6
bababuck <92571492+bababuck@users.noreply.github.com>
2025-07-05 15:27:04 -0700

    Initial bare flow that compiles

---
+++ lib/Dialect/CuttingBored/CuttingBoredDialect.cpp
@@ -0,0 +1,15 @@
+#include "lib/Dialect/CuttingBored/CuttingBoredDialect.h"
+
+#include "mlir/include/mlir/IR/Builders.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"
+
+#include "lib/Dialect/CuttingBored/CuttingBoredDialect.cpp.inc"
+
+namespace mlir {
+namespace cuttingbored {
+
+void CuttingBoredDialect::initialize() {
+}
+
+} // namespace cuttingbored
+} // namespace mlir
---
+++ lib/Dialect/CuttingBored/CuttingBoredDialect.h
@@ -0,0 +1,10 @@
+#ifndef LIB_DIALECT_CUTTINGBORED_CUTTINGBOREDDIALECT_H_
+#define LIB_DIALECT_CUTTINGBORED_CUTTINGBOREDDIALECT_H_
+
+// Required because the .h.inc file refers to MLIR classes and does not itself
+// have any includes.
+#include "mlir/include/mlir/IR/DialectImplementation.h"
+
+#include "lib/Dialect/CuttingBored/CuttingBoredDialect.h.inc"
+
+#endif  // LIB_DIALECT_CUTTINGBORED_CUTTINGBOREDDIALECT_H_
