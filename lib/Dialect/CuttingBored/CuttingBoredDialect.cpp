e8b42b1eb9814acb55f2ef03636b093d34aaaffc
bababuck <92571492+bababuck@users.noreply.github.com>
2025-07-06 23:44:15 -0700

    Add new type !CuttingBored.board

--- lib/Dialect/CuttingBored/CuttingBoredDialect.cpp
+++ lib/Dialect/CuttingBored/CuttingBoredDialect.cpp
@@ -1,14 +1,21 @@
 #include "lib/Dialect/CuttingBored/CuttingBoredDialect.h"
 
+#include "lib/Dialect/CuttingBored/CuttingBoredTypes.h"
 #include "mlir/include/mlir/IR/Builders.h"
 #include "llvm/include/llvm/ADT/TypeSwitch.h"
 
 #include "lib/Dialect/CuttingBored/CuttingBoredDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/CuttingBored/CuttingBoredTypes.cpp.inc"
 
 namespace mlir {
 namespace cuttingbored {
 
 void CuttingBoredDialect::initialize() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "lib/Dialect/CuttingBored/CuttingBoredTypes.cpp.inc"
+    >();
 }
 
 } // namespace cuttingbored
