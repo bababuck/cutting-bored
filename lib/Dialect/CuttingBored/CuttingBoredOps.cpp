dfa447e9bf1865e09fb7c07565f4f301bf4ee158
bababuck <92571492+bababuck@users.noreply.github.com>
2025-07-11 16:43:44 -0700

    Rename Build -> BuildOp and InsertColumn -> InsertColumnOp
    Clarify when working with the CPP objects

--- lib/Dialect/CuttingBored/CuttingBoredOps.cpp
+++ lib/Dialect/CuttingBored/CuttingBoredOps.cpp
@@ -9,11 +9,11 @@ namespace mlir {
 namespace cuttingbored {
 
 // Remove a column if solid color
-struct InsertColumnMatch : public OpRewritePattern<Build> {
+struct InsertColumnMatch : public OpRewritePattern<BuildOp> {
   InsertColumnMatch(mlir::MLIRContext *context)
-      : OpRewritePattern<Build>(context, /*benefit=*/1) {}
+      : OpRewritePattern<BuildOp>(context, /*benefit=*/1) {}
 
-  virtual LogicalResult matchAndRewrite(Build op,
+  virtual LogicalResult matchAndRewrite(BuildOp op,
                                 PatternRewriter &rewriter) const override {
     Value InputBoard = op.getToBuild();
 
@@ -26,7 +26,7 @@ struct InsertColumnMatch : public OpRewritePattern<Build> {
     auto Squares = InputBoardType.getSquares();
     if (Columns == 0)
       return failure();
-    llvm::errs() << Rows << "HEHE\n";
+
     uint64_t RowAnd = -1;
     uint64_t RowOr = 0;
     for (auto Row : Squares) {
@@ -62,24 +62,14 @@ struct InsertColumnMatch : public OpRewritePattern<Build> {
     BoardType NewBoardType = BoardType::get(rewriter.getContext(), Rows, Columns - 1, NewBoardSquares);
     BoardType NewColumnType = BoardType::get(rewriter.getContext(), Rows, 1, NewBoardSquares);
     Operation *NewBuild = rewriter.create(op.getLoc(), rewriter.getStringAttr("cuttingbored.build"), {}, {NewBoardType});
-    llvm::errs() << "HEHE\n";
     Operation *NewColumn = rewriter.create(op.getLoc(), rewriter.getStringAttr("cuttingbored.column"), {}, {NewColumnType});
-    //Build NewBuild = rewriter.create<Build>(op.getLoc(), {NewBoardType});
-    // ColumnOp NewColumn = rewriter.create<ColumnOp>(op.getLoc(), {NewColumnType});
     Operation *NewInsertColumn = rewriter.create(op.getLoc(), rewriter.getStringAttr("cuttingbored.insert_column"), {NewBuild->getResult(0), NewColumn->getResult(0)}, {InputBoardType});
-    /*
-    AddOp newAdd = rewriter.create<AddOp>(op.getLoc(), x, y);
-    SubOp newSub = rewriter.create<SubOp>(op.getLoc(), x, y);
-    MulOp newMul = rewriter.create<MulOp>(op.getLoc(), newAdd, newSub);
-    */
     rewriter.replaceOp(op, NewInsertColumn);
-    // We don't need to remove the original ops because MLIR already has
-    // canonicalization patterns that remove unused ops.
     return success();
   }
 };
 
-void Build::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
+void BuildOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
                                         ::mlir::MLIRContext *context) {
   results.add<InsertColumnMatch>(context);
 }
